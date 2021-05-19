deeec6fd19273cbab071d8e70bbce81c119432b7
mariagluck <mariamart.berlin@gmail.com>
2021-05-19 09:56:45 +0200

    updates tick tock

--- classes.js
+++ classes.js
@@ -22,10 +22,10 @@ personOne.describe(); //John, 19 years old
  class Clock {
     constructor({ template }) {
       this.template = template;
-    
+    }
   
-        function render() {
-          let date = new Date();
+     render() {
+          const date = new Date();
       
           let hours = date.getHours();
           if (hours < 10) hours = '0' + hours;
@@ -36,26 +36,26 @@ personOne.describe(); //John, 19 years old
           let secs = date.getSeconds();
           if (secs < 10) secs = '0' + secs;
       
-          let output = template
+          let output = this.template
             .replace('h', hours)
             .replace('m', mins)
             .replace('s', secs);
       
           console.log(output);
-        }
-    
-        this.stop = function() {
+     }
+
+     stop = function() {
             // do nothing!
-        };
-        this.start = function() {
-            render();
-        };
+     };
+     start = function() {
+            this.render();
+     }
     
-   }
-  
 }
   
-let clock = new Clock({template: 'h:m:s'});
+
+  
+const clock = new Clock({template: 'h:m:s'});
 clock.start();
 
 
